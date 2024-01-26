f8b0a80214661fde2229aa3abb25cbcc042d9222
Brandon Catubig <bcatubig@pm.me>
2024-01-26 15:18:12 -0800

    complete hamming solution

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,17 @@
 #include "hamming.h"
+#include <string.h>
+int compute(const char *lhs, const char *rhs) {
+  int diff = 0;
+
+  if (strlen(lhs) != strlen(rhs)) {
+    return -1;
+  }
+
+  for (int i = 0; i < (int)strlen(lhs); i++) {
+    if (lhs[i] != rhs[i]) {
+      diff++;
+    }
+  }
+
+  return diff;
+}
