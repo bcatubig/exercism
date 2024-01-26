7f7f4bd739cd3d749801536a159ed74b9c9527da
Brandon Catubig <bcatubig@pm.me>
2024-01-26 13:35:44 -0800

    add isogram solution

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,34 @@
 #include "isogram.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_isogram(const char *phrase) {
+  if (phrase == NULL) {
+    return false;
+  }
+
+  if (*phrase == '\0') {
+    return true;
+  }
+
+  int phrase_len = strlen(phrase);
+
+  for (int i = 0; i < phrase_len - 1; i++) {
+    if (phrase[i] == '-' || phrase[i] == ' ') {
+      continue;
+    }
+    for (int j = i + 1; j < phrase_len; j++) {
+      if (phrase[j] == '-' || phrase[j] == ' ') {
+        continue;
+      }
+
+      if (tolower(phrase[i]) == tolower(phrase[j])) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
