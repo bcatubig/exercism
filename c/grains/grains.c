0b7b8e5f846b6957aa2390c248f6b304df15f3d6
Brandon Catubig <bcatubig@pm.me>
2024-01-26 15:42:09 -0800

    add grains solution

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,30 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index) {
+  // 1 - 1
+  // 2 - 2
+  // 3 - 4
+  // 4 - 8
+  // 16 - 32768
+  // 32 - 2147483648
+
+  if (index == 0) {
+    return 0;
+  }
+
+  if (index == 1) {
+    return 1;
+  }
+
+  return square(index - 1) * 2;
+}
+
+uint64_t total(void) {
+  uint64_t result = 0;
+
+  for (int i = 1; i <= 64; i++) {
+    result += square(i);
+  }
+
+  return result;
+}
